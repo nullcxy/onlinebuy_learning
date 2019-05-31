366c03b39c0d08df80bfff22516032f9762b28ba
nullcxy <1287530995@qq.com>
2019-05-31 16:01:14 +0800

    网上购物用户中心

---
+++ onlinebuy-user-service/src/main/java/com/cxy/OnlinebuyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebuyUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinebuyUserServiceApplication.class, args);
+    }
+
+}
